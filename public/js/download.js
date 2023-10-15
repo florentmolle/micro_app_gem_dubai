dadb736b937aed04d83f59f54e177d430e8c7d9d
florent molle <florent-molle@hotmail.fr>
2023-10-15 12:46:57 +0700

    update

--- public/js/download.js
+++ public/js/download.js
@@ -10,7 +10,7 @@ window.addEventListener('load', ()=>{
             let path = btn.getAttribute('data-path');
 
             console.log('path : ', path)
-            link.href = `https://geminclusion-microapp-e9da7c5a12da.herokuapp.com/${path}`; // replace with your actual file path
+            link.href = path; // replace with your actual file path
             let imageName = path.replace('uploads/', "");
             link.download = imageName; // replace with your actual file name
             link.click();
